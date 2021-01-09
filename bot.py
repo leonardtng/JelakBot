d46136882fb95130d6636e2e86f65c060672271c
Leonard Tng <leonardtng@gmail.com>
2021-01-09 11:07:56 +0800

    Cleaned up code

--- bot.py
+++ bot.py
@@ -8,15 +8,18 @@ import csv
 from credentials import TOKEN
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
 LOCATION, RESTAURANT, ORDER_RESTAURANT, ORDER_LOCATION, CUTOFF, VIEW, ORDERS_LIST = range(7)
 
-LOCATION_LIST = ['Yale-NUS', 'Tembusu College', 'RC 4', 'College of Alice and Peter Tan', 'Temasek Hall']
-RESTAURANT_LIST = ['McDonalds', 'Pastamania']
+LOCATION_LIST = ["Yale-NUS", "Tembusu College", "RC 4", "College of Alice and Peter Tan", "Temasek Hall"]
+RESTAURANT_LIST = ["McDonalds", "Pastamania", "Dominos", "Al-Amans", "Koi"]
+
+LOCATION_REGEX = "^(Yale-NUS|Tembusu College|RC 4|College of Alice and Peter Tan|Temasek Hall)$"
+RESTAURANT_REGEX = "^(McDonalds|Pastamania|Dominos|Al-Amans|Koi)$"
 
 """
 START STREAM
@@ -30,11 +33,11 @@ user_data = {
 
 
 def start(update, context):
-    print('START')
+    print("START")
     reply_keyboard = [LOCATION_LIST]
 
     update.message.reply_text(
-        'Welcome to JelakBot! Please select your location',
+        "Welcome to JelakBot! Please select your location",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     )
 
@@ -46,15 +49,15 @@ def location(update, context):
     user = update.message.from_user
     logger.info("%s chose: %s", user.first_name, update.message.text)
 
-    update.message.reply_text(f'Location saved as {update.message.text}')
+    update.message.reply_text(f"Location saved as {update.message.text}")
 
-    user_data["chat_id"] = update.message['chat']["id"]
+    user_data["chat_id"] = update.message["chat"]["id"]
     user_data["location"] = update.message.text
 
     reply_keyboard = [RESTAURANT_LIST]
     update.message.reply_text(
-        'Please select restaurants which you want to receive notifications for. You will only be notified when new '
-        'requests are made for those those restaurants',
+        "Please select restaurants which you want to receive notifications for. You will only be notified when new "
+        "requests are made for those those restaurants",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     )
 
@@ -67,27 +70,27 @@ def restaurant(update, context):
 
     logger.info("%s chose: %s", user.first_name, update.message.text)
 
-    with open('user_file.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',', fieldnames=('chat_id', 'location', 'restaurants'))
+    with open("user_file.csv", mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=",", fieldnames=("chat_id", "location", "restaurants"))
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             elif row["chat_id"] == str(user_data["chat_id"]):
-                update.message.reply_text('You have already signed up!', reply_markup=ReplyKeyboardRemove())
+                update.message.reply_text("You have already signed up!", reply_markup=ReplyKeyboardRemove())
                 return ConversationHandler.END
             else:
                 line_count += 1
 
-    update.message.reply_text(f'Restaurant saved as {update.message.text}', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(f"Restaurant saved as {update.message.text}", reply_markup=ReplyKeyboardRemove())
 
     user_data["restaurants"] = update.message.text
 
     print(user_data)
 
-    with open('user_file.csv', mode='a', newline='') as user_file:
-        fieldnames = ['chat_id', 'location', 'restaurants']
+    with open("user_file.csv", mode="a", newline="") as user_file:
+        fieldnames = ["chat_id", "location", "restaurants"]
         writer = csv.DictWriter(user_file, fieldnames=fieldnames)
 
         writer.writerow({"chat_id": user_data["chat_id"], "location": user_data["location"],
@@ -108,11 +111,11 @@ order_data = {
 
 
 def order(update, context):
-    print('ORDER')
+    print("ORDER")
     reply_keyboard = [RESTAURANT_LIST]
 
     update.message.reply_text(
-        'Please select the restaurant you are ordering from.',
+        "Please select the restaurant you are ordering from.",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     )
 
@@ -124,15 +127,15 @@ def order_restaurant(update, context):
 
     logger.info("%s chose: %s", user.first_name, update.message.text)
 
-    update.message.reply_text(f'Order Restaurant saved as {update.message.text}', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(f"Order Restaurant saved as {update.message.text}", reply_markup=ReplyKeyboardRemove())
 
-    order_data["poc_username"] = update.message['chat']["username"]
+    order_data["poc_username"] = update.message["chat"]["username"]
     order_data["order_restaurant"] = update.message.text
 
     reply_keyboard = [LOCATION_LIST]
 
     update.message.reply_text(
-        'Please select where you are delivering your food to.',
+        "Please select where you are delivering your food to.",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     )
 
@@ -144,12 +147,12 @@ def order_location(update, context):
 
     logger.info("%s chose: %s", user.first_name, update.message.text)
 
-    update.message.reply_text(f'Order Location saved as {update.message.text}', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(f"Order Location saved as {update.message.text}", reply_markup=ReplyKeyboardRemove())
 
     order_data["order_location"] = update.message.text
 
     update.message.reply_text(
-        'Please indicate the cutoff time which afterwards people can no longer join your order. ',
+        "Please indicate the cutoff time which afterwards people can no longer join your order. ",
     )
 
     return CUTOFF
@@ -160,21 +163,21 @@ def cutoff(update, context):
 
     logger.info("%s order cutoff: %s", user.first_name, update.message.text)
 
-    update.message.reply_text(f'Order Cutoff saved as {update.message.text}')
+    update.message.reply_text(f"Order Cutoff saved as {update.message.text}")
 
     order_data["cutoff"] = update.message.text
 
     print(order_data)
 
-    update.message.reply_text('Your order is saved as:\n'
+    update.message.reply_text("Your order is saved as:\n"
                               f'Restuarant: {order_data["order_restaurant"]}\n'
                               f'Order Location: {order_data["order_location"]}\n'
                               f'Cutoff Time: {order_data["cutoff"]}\n'
                               f'POC Username: @{order_data["poc_username"]}\n'
                               'Please be reminded that your telegram handle will be shared.')
 
-    with open('orders_file.csv', mode='a', newline='') as orders_file:
-        fieldnames = ['order_restaurant', 'order_location', 'cutoff', 'poc_username']
+    with open("orders_file.csv", mode="a", newline="") as orders_file:
+        fieldnames = ["order_restaurant", "order_location", "cutoff", "poc_username"]
         writer = csv.DictWriter(orders_file, fieldnames=fieldnames)
 
         writer.writerow(
@@ -183,8 +186,8 @@ def cutoff(update, context):
              "cutoff": order_data["cutoff"],
              "poc_username": order_data["poc_username"]})
 
-    with open('user_file.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',', fieldnames=('chat_id', 'location', 'restaurants'))
+    with open("user_file.csv", mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=",", fieldnames=("chat_id", "location", "restaurants"))
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
@@ -213,11 +216,11 @@ VIEW STREAM
 
 
 def view(update, context):
-    print('VIEW')
+    print("VIEW")
     reply_keyboard = [LOCATION_LIST]
 
     update.message.reply_text(
-        'Please select your current location.',
+        "Please select your current location.",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     )
 
@@ -228,11 +231,11 @@ def orders_list(update, context):
     user = update.message.from_user
     logger.info("%s chose: %s", user.first_name, update.message.text)
 
-    update.message.reply_text(f'Displaying orders from {update.message.text}:', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(f"Displaying orders from {update.message.text}:", reply_markup=ReplyKeyboardRemove())
 
-    with open('orders_file.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',', fieldnames=('order_restaurant', 'order_location', 'cutoff',
-                                                                         'poc_username'))
+    with open("orders_file.csv", mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=",", fieldnames=("order_restaurant", "order_location", "cutoff",
+                                                                         "poc_username"))
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
@@ -251,16 +254,16 @@ def orders_list(update, context):
     return ConversationHandler.END
 
 
-'''
+"""
 HELPERS
-'''
+"""
 
 
 def help(update, context):
-    help_message = ('Use these commands to navigate JelakBot:\n'
-                    '/start - Set up your location and preferred restaurant which you would like notifications for\n'
-                    '/order - Start an order\n'
-                    '/view - View orders in your location'
+    help_message = ("Use these commands to navigate JelakBot:\n"
+                    "/start - Set up your location and preferred restaurant which you would like notifications for\n"
+                    "/order - Start an order\n"
+                    "/view - View orders in your location"
                     )
 
     update.message.reply_text(help_message)
@@ -272,41 +275,41 @@ def main():
     dp = updater.dispatcher
 
     conv_handler_signup = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler("start", start)],
         fallbacks=[],
 
         states={
             LOCATION: [MessageHandler(
-                Filters.regex('^(Yale-NUS|Tembusu College|RC 4|College of Alice and Peter Tan|Temasek Hall)$'),
+                Filters.regex(LOCATION_REGEX),
                 location)],
             RESTAURANT: [MessageHandler(
-                Filters.regex('^(McDonalds|Pastamania)$'),
+                Filters.regex(RESTAURANT_REGEX),
                 restaurant)],
         },
     )
 
     conv_handler_order = ConversationHandler(
-        entry_points=[CommandHandler('order', order)],
+        entry_points=[CommandHandler("order", order)],
         fallbacks=[],
 
         states={
             ORDER_RESTAURANT: [MessageHandler(
-                Filters.regex('^(McDonalds|Pastamania)$'),
+                Filters.regex(RESTAURANT_REGEX),
                 order_restaurant)],
             ORDER_LOCATION: [MessageHandler(
-                Filters.regex('^(Yale-NUS|Tembusu College|RC 4|College of Alice and Peter Tan|Temasek Hall)$'),
+                Filters.regex(LOCATION_REGEX),
                 order_location)],
             CUTOFF: [MessageHandler(Filters.text & ~Filters.command, cutoff)],
         },
     )
 
     conv_handler_view = ConversationHandler(
-        entry_points=[CommandHandler('view', view)],
+        entry_points=[CommandHandler("view", view)],
         fallbacks=[],
 
         states={
             ORDERS_LIST: [MessageHandler(
-                Filters.regex('^(Yale-NUS|Tembusu College|RC 4|College of Alice and Peter Tan|Temasek Hall)$'),
+                Filters.regex(LOCATION_REGEX),
                 orders_list)],
         },
     )
@@ -320,5 +323,5 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
